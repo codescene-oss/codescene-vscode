fa9e17d761ac05b48322f83ca15d7a4de4b2eab2
Simon Sandlund <simon.sandlund@gmail.com>
2023-08-17 10:08:52 +0200

    Reformat code

--- src/csdoc.ts
+++ src/csdoc.ts
@@ -3,35 +3,33 @@ import { join } from 'path';
 import * as vscode from 'vscode';
 
 class CsDocProvider implements vscode.TextDocumentContentProvider {
-    logoUrl: string | undefined;
+  logoUrl: string | undefined;
 
-    constructor(private docPath: string) {
+  constructor(private docPath: string) {}
 
+  async getLogoUrl(): Promise<string> {
+    if (!this.logoUrl) {
+      // Read the logo from the extension's assets folder and base64 encode it.
+      const path = join(this.docPath, '..', 'assets', 'cs-logo-small.png');
+      const data = await readFile(path);
+      this.logoUrl = data.toString('base64');
     }
+    return this.logoUrl;
+  }
 
-    async getLogoUrl(): Promise<string> {
-        if (!this.logoUrl) {
-            // Read the logo from the extension's assets folder and base64 encode it.
-            const path = join(this.docPath, '..', 'assets', 'cs-logo-small.png');
-            const data = await readFile(path);
-            this.logoUrl = data.toString('base64');
-        }
-        return this.logoUrl;
-    }
+  async provideTextDocumentContent(uri: vscode.Uri): Promise<string> {
+    const url = await this.getLogoUrl();
+    const header = `# <img src="data:image/png;base64,${url}" width="64" height="64" align="center" />&nbsp; `;
 
-    async provideTextDocumentContent(uri: vscode.Uri): Promise<string> {
-        const url = await this.getLogoUrl();
-        const header = `# <img src="data:image/png;base64,${url}" width="64" height="64" align="center" />&nbsp; `;
+    const path = join(this.docPath, uri.path);
+    const content = await readFile(path);
 
-        const path = join(this.docPath, uri.path);
-        const content = await readFile(path);
-
-        return header + content.toString();
-    }
+    return header + content.toString();
+  }
 }
 
 export function registerCsDocProvider(docPath: string) {
-    const provider = new CsDocProvider(docPath);
-    const providerRegistration = vscode.workspace.registerTextDocumentContentProvider('csdoc', provider);
-    return providerRegistration;
+  const provider = new CsDocProvider(docPath);
+  const providerRegistration = vscode.workspace.registerTextDocumentContentProvider('csdoc', provider);
+  return providerRegistration;
 }
