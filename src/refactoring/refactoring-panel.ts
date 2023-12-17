4f81a3e6ad1003e26f3a5df4b6399c3dbdfb3da9
Johan Lindbergh <johan.lindbergh@codescene.com>
2023-12-17 08:02:01 +0100

    Add webview-script and adapt esbuild for new structure
    Mostly following along with https://github.com/microsoft/vscode-webview-ui-toolkit/blob/main/docs/getting-started.md
    Align loading and content webview panels with webview-ui-components

--- src/refactoring/refactoring-panel.ts
+++ src/refactoring/refactoring-panel.ts
@@ -16,13 +16,14 @@ export class RefactoringPanel {
 
   private currentRefactorSuggestion: RefactoringSuggestion | undefined;
 
-  public constructor() {
-    this.webViewPanel = vscode.window.createWebviewPanel(
+  public constructor(extensionUri: vscode.Uri) {
+    this.webViewPanel = window.createWebviewPanel(
       RefactoringPanel.viewType,
       'CodeScene AI Refactor',
       RefactoringPanel.column,
       {
         enableScripts: true,
+        localResourceRoots: [vscode.Uri.joinPath(extensionUri, 'out'), vscode.Uri.joinPath(extensionUri, 'assets')],
       }
     );
 
@@ -36,6 +37,7 @@ export class RefactoringPanel {
             return;
           case 'reject':
             this.dispose();
+            return;
         }
       },
       null,
@@ -54,88 +56,67 @@ export class RefactoringPanel {
     workspace.applyEdit(workSpaceEdit);
   }
 
-  private update(extensionUri: vscode.Uri, document: vscode.TextDocument, request: RefactorRequest, response?: RefactorResponse) {
-    if (!response) {
-      this.setupLoadingView(extensionUri);
-      return;
-    }
+  private async updateWebView(
+    extensionUri: vscode.Uri,
+    document: vscode.TextDocument,
+    request: RefactorRequest,
+    response?: RefactorResponse
+  ) {
+    const styleUri = getUri(this.webViewPanel.webview, extensionUri, ['assets', 'refactor-styles.css']);
+    const webviewScript = getUri(this.webViewPanel.webview, extensionUri, ['out', 'webview-script.js']);
+    const csLogoUrl = await getLogoUrl(extensionUri.fsPath);
+    const content = response ? this.getContent(document, request, response) : this.getLoadingContent(extensionUri);
+    // Note, the html "typehint" is used by the es6-string-html extension to enable highlighting of the html-string
+    this.webViewPanel.webview.html = /*html*/ `
+    <!DOCTYPE html>
+    <html lang="en">
 
+    <head>
+        <meta charset="UTF-8">
+        <link href="${styleUri}" rel="stylesheet" />
+    </head>
+
+    <body>
+        <script type="module" nonce="${nonce}" src="${webviewScript}"></script>
+        <h1><img src="data:image/png;base64,${csLogoUrl}" width="64" height="64" align="center"/>&nbsp; Refactoring recommendation</h1>
+        <vscode-divider></vscode-divider>
+        ${content}
+    </body>
+
+    </html>
+    `;
+  }
+
+  private getContent(document: vscode.TextDocument, request: RefactorRequest, response: RefactorResponse) {
     let { code, reasons, confidence } = response;
     code = code.trim(); // Service might have returned code with extra whitespace. Trim to make it match startLine when replacing
     const { start_line: startLine, end_line: endLine } = request.source_snippet;
     const range = new vscode.Range(startLine, 0, endLine, 0);
     this.currentRefactorSuggestion = { documentToEdit: document, code, range };
 
-    this.setupWebView(extensionUri, confidence, reasons, code);
-  }
-
-  private async setupLoadingView(extensionUri: vscode.Uri) {
-    const csLogoUrl = await getLogoUrl(extensionUri.fsPath);
-    const styleUri = this.webViewPanel.webview.asWebviewUri(
-      vscode.Uri.joinPath(extensionUri, 'assets', 'refactor-styles.css')
-    );
-
-    this.webViewPanel.webview.html = /*html*/`
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <link href="${styleUri}" rel="stylesheet" />
-</head>
-<body>
-    <h1><img src="data:image/png;base64,${csLogoUrl}" width="64" height="64" align="center"/>&nbsp; Refactoring recommendation</h1>
-    <h2>Loading refactoring...</h2>
-</body>
-</html>
-`;
-  }
-
-  private async setupWebView(
-    extensionUri: vscode.Uri,
-    confidence: RefactorConfidence,
-    reasons: string[],
-    code: string
-  ) {
     const { level, description } = confidence;
     const reasonText = reasons.join('. ');
-    const styleUri = this.webViewPanel.webview.asWebviewUri(
-      vscode.Uri.joinPath(extensionUri, 'assets', 'refactor-styles.css')
-    );
-    const csLogoUrl = await getLogoUrl(extensionUri.fsPath);
 
-    // Note, the html "typehint" is used by the es6-string-html extension to enable highlighting of the html-string
-    this.webViewPanel.webview.html = /*html*/`
-<!DOCTYPE html>
-<html lang="en">
+    const acceptDefault = level >= 2;
 
-<head>
-    <meta charset="UTF-8">
-    <link href="${styleUri}" rel="stylesheet" />
-    <script nonce="${nonce}">
-        const vscode = acquireVsCodeApi();
-        function sendMessage(cmd) {
-            const message = { command: cmd, text: 'Hello from webview!' };
-            vscode.postMessage(message);
-        }
-    </script>
-</head>
+    return /*html*/ `
+      <h2>Confidence score</h2>
+      <vscode-tag>${description}</vscode-tag>
+      <div class="reasons">${reasonText}</div>
+      <h2>Proposed change</h2>
+      <div>
+        <pre><code>${code}</code></pre>
+      </div>
+      <div class="buttons">
+        <vscode-button id="reject-button" appearance="${acceptDefault ? 'secondary' : 'primary'}">Reject</vscode-button>
+        <vscode-button id="apply-button" appearance="${acceptDefault ? 'primary' : 'secondary'}">Apply</vscode-button>
+      </div>
+  `;
+  }
 
-<body>
-    <h1><img src="data:image/png;base64,${csLogoUrl}" width="64" height="64" align="center"/>&nbsp; Refactoring recommendation</h1>
-    <h2>Confidence score</h2>
-    <div class="confidence-label confidence-${level}">${description}</div>
-    <div class="reasons">${reasonText}</div>
-    <div>
-      <pre><code>${code}</code></pre>
-    </div>
-    <div class="buttons">
-      <button class="reject" onclick="sendMessage('reject')">Reject</button>
-      <button class="apply" onclick="sendMessage('apply')">Apply</button>
-    </div>
-</body>
-
-</html>
-`;
+  private getLoadingContent(extensionUri: vscode.Uri) {
+    return /*html*/ `<h2>Loading refactoring...</h2>
+    <vscode-progress-ring></vscode-progress-ring>`;
   }
 
   public dispose() {
@@ -157,17 +138,27 @@ export class RefactoringPanel {
    * @param response
    * @returns
    */
-  public static createOrShow(extensionUri: vscode.Uri, document: vscode.TextDocument, request: RefactorRequest, response?: RefactorResponse) {
+  public static createOrShow(
+    extensionUri: vscode.Uri,
+    document: vscode.TextDocument,
+    request: RefactorRequest,
+    response?: RefactorResponse
+  ) {
     if (RefactoringPanel.currentPanel) {
-      RefactoringPanel.currentPanel.update(extensionUri, document, request, response);
+      RefactoringPanel.currentPanel.updateWebView(extensionUri, document, request, response);
       RefactoringPanel.currentPanel.webViewPanel.reveal(RefactoringPanel.column);
       return;
     }
 
     // Otherwise, create a new web view panel.
-    RefactoringPanel.currentPanel = new RefactoringPanel();
-    RefactoringPanel.currentPanel.update(extensionUri, document, request, response);
+    RefactoringPanel.currentPanel = new RefactoringPanel(extensionUri);
+    RefactoringPanel.currentPanel.updateWebView(extensionUri, document, request, response);
   }
+}
+
+// Webview utility functions below
+function getUri(webview: vscode.Webview, extensionUri: vscode.Uri, pathList: string[]) {
+  return webview.asWebviewUri(vscode.Uri.joinPath(extensionUri, ...pathList));
 }
 
 function nonce() {
