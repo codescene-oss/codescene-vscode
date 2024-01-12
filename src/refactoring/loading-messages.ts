15a80df0e81dc125e94b221df44eaa950745d43b
Johan Lindbergh <johan.lindbergh@codescene.com>
2024-01-12 14:50:03 +0100

    Test out some different loading messages

---
+++ src/refactoring/loading-messages.ts
@@ -0,0 +1,13 @@
+export default [
+  'Preparing refactoring',
+  'Asking around for refactoring suggestions',
+  'Finding optimal AI service',
+  'Parsing suggested code',
+  'Validating suggested refactoring',
+  'Performing syntactic validation',
+  'Using up all 640Kbs of RAM',
+  'Spinning violently around the y-axis',
+  'Accepting cookies',
+  'Avoiding bluescreen',
+  'Convincing AI not to turn evil',  
+];
