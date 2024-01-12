ed87f68936c03115bd9e6a2ac9d8afb691f81b1d
Johan Lindbergh <johan.lindbergh@codescene.com>
2024-01-12 11:03:13 +0100

    Add toggle preview checkbox to quickly see changes

--- src/refactoring/webview-script.ts
+++ src/refactoring/webview-script.ts
@@ -3,18 +3,20 @@ import {
   vsCodeButton,
   vsCodeDivider,
   vsCodeProgressRing,
+  vsCodeCheckbox,
 } from '@vscode/webview-ui-toolkit';
 
-provideVSCodeDesignSystem().register(vsCodeButton(), vsCodeDivider(), vsCodeProgressRing());
+provideVSCodeDesignSystem().register(vsCodeButton(), vsCodeDivider(), vsCodeProgressRing(), vsCodeCheckbox());
 
 const vscode = acquireVsCodeApi();
 
 window.addEventListener('load', main);
 
 function main() {
+  document.getElementById('diff-button')?.addEventListener('click', () => sendMessage('show-diff'));
   document.getElementById('reject-button')?.addEventListener('click', () => sendMessage('reject'));
   document.getElementById('apply-button')?.addEventListener('click', () => sendMessage('apply'));
-  document.getElementById('diff-button')?.addEventListener('click', () => sendMessage('show-diff'));
+  document.getElementById('toggle-apply')?.addEventListener('click', () => sendMessage('toggle-apply'));
   document.getElementById('copy-to-clipboard')?.addEventListener('click', () => sendMessage('copy-code'));
 }
 
