706adaf6c08df9494daaf588892b6622d05c66b6
Johan Lindbergh <johan.lindbergh@codescene.com>
2024-01-23 18:52:21 +0100

    Adapt to updated refactoring API and enclosing-function cli cmd

--- src/refactoring/command.ts
+++ src/refactoring/command.ts
@@ -1,16 +1,29 @@
 import vscode, { DocumentSymbol, SymbolKind, TextDocument, Uri, commands, window } from 'vscode';
 import { RefactoringPanel } from './refactoring-panel';
-import { CsRestApi } from '../cs-rest-api';
+import { CsRestApi, RefactorRequest } from '../cs-rest-api';
 import axios, { AxiosError } from 'axios';
+import { findEnclosingFunction } from '../codescene-interop';
+import { env } from 'process';
 
 export const name = 'codescene.requestRefactoring';
 
+export interface FnToRefactor {
+  name: string;
+  range: vscode.Range;
+  content: string;
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  'file-type': string;
+  functionType: string;
+}
+
 export class CsRefactoringCommand {
   private readonly csRestApi: CsRestApi;
+  private readonly cliPath: string;
   private abortController: AbortController | undefined;
 
-  constructor(csRestApi: CsRestApi) {
+  constructor(csRestApi: CsRestApi, cliPath: string) {
     this.csRestApi = csRestApi;
+    this.cliPath = cliPath;
   }
 
   /**
@@ -29,8 +42,8 @@ export class CsRefactoringCommand {
     diagnostics: vscode.Diagnostic[]
   ) {
     const diagnostic = diagnostics[0];
-    const fn = await findFunctionToRefactor(document, diagnostic.range);
-    if (!fn) {
+    const fnToRefactor = await findFunctionToRefactor(this.cliPath, document, diagnostic.range);
+    if (!fnToRefactor) {
       console.error('CodeScene: Could not find a suitable function to refactor.');
       window.showErrorMessage('Could not find a suitable function to refactor.');
       return;
@@ -38,23 +51,23 @@ export class CsRefactoringCommand {
 
     const editor = window.activeTextEditor;
     if (editor) {
-      editor.selection = new vscode.Selection(fn.range.start, fn.range.end);
-      editor.revealRange(fn.range, vscode.TextEditorRevealType.InCenterIfOutsideViewport);
+      editor.selection = new vscode.Selection(fnToRefactor.range.start, fnToRefactor.range.end);
+      editor.revealRange(fnToRefactor.range, vscode.TextEditorRevealType.InCenterIfOutsideViewport);
     }
-    const extensionUri = context.extensionUri;
-    const request = refactorRequest(document, diagnostic, fn);
     const initiatorViewColumn = editor?.viewColumn;
 
     // Send abort signal to currently running refactoring request (if any)
     if (this.abortController) this.abortController.abort();
 
     this.abortController = new AbortController(); // New abort controller for the new request
-    console.log(`CodeScene: Requesting refactoring suggestion for "${fn.name}" from CodeScene's AI service`);
-    RefactoringPanel.createOrShow({ extensionUri, document, initiatorViewColumn, fnToRefactor: fn });
+    console.log(`CodeScene: Requesting refactoring suggestion for "${fnToRefactor.name}" from CodeScene's AI service`);
+
+    const extensionUri = context.extensionUri;
+    RefactoringPanel.createOrShow({ extensionUri, document, initiatorViewColumn, fnToRefactor });
     this.csRestApi
-      .fetchRefactoring(request, this.abortController.signal)
+      .fetchRefactoring(diagnostic, fnToRefactor, this.abortController.signal)
       .then((response) => {
-        RefactoringPanel.createOrShow({ extensionUri, document, initiatorViewColumn, fnToRefactor: fn, response });
+        RefactoringPanel.createOrShow({ extensionUri, document, initiatorViewColumn, fnToRefactor, response });
       })
       .catch((err: Error | AxiosError) => {
         if (err instanceof AxiosError && axios.isCancel(err)) {
@@ -66,63 +79,37 @@ export class CsRefactoringCommand {
           extensionUri,
           document,
           initiatorViewColumn,
-          fnToRefactor: fn,
+          fnToRefactor,
           response: err.message,
         });
       });
   }
 }
 
-function refactorRequest(
-  document: vscode.TextDocument,
-  diagnostic: vscode.Diagnostic,
-  fn: DocumentSymbol
-): RefactorRequest {
-  const review: Review = {
-    category: codeToCategory(diagnostic.code),
-    start_line: diagnostic.range.start.line,
-  };
+async function findFunctionToRefactor(cliPath: string, document: TextDocument, range: vscode.Range) {
+  const extension = document.fileName.split('.').pop() || '';
+  const enclosingFn = await findEnclosingFunction(
+    cliPath,
+    extension,
+    range.start.line + 1, // range.start.line is zero-based
+    document.getText()
+  );
 
-  const sourceSnippet: SourceSnippet = {
-    language: 'JavaScript',
-    start_line: fn.range.start.line,
-    end_line: fn.range.end.line + 1, // +1 because we want the linebreak at the end
-    content: document.getText(fn.range),
-  };
-  return { review: [review], source_snippet: sourceSnippet };
-}
+  if (!enclosingFn) return;
 
-function codeToCategory(diagnosticCode: string | number | { value: string | number; target: Uri } | undefined) {
-  if (typeof diagnosticCode === 'object') {
-    return diagnosticCode.value.toString();
-  }
-  return 'unknown category';
-}
+  // Note that vscode.Range line numbers are zero-based
+  const enclosingFnRange = new vscode.Range(
+    enclosingFn['start-line'] - 1,
+    enclosingFn['start-column'],
+    enclosingFn['end-line'] - 1,
+    enclosingFn['end-column']
+  );
 
-const symbolsToFind = [SymbolKind.Function, SymbolKind.Method];
-const symbolFilter = (symbol: DocumentSymbol) => symbolsToFind.includes(symbol.kind);
-
-function getFilteredSymbols(symbols: DocumentSymbol[]) {
-  const filteredSymbols = symbols.filter(symbolFilter);
-  const childSymbols = symbols.flatMap((s) => getFilteredSymbols(s.children));
-  filteredSymbols.push(...childSymbols);
-  return filteredSymbols;
-}
-
-async function findFunctionToRefactor(document: TextDocument, range: vscode.Range) {
-  const docSymbols = (await commands.executeCommand('vscode.executeDocumentSymbolProvider', document.uri)) as
-    | DocumentSymbol[]
-    | undefined;
-
-  if (!docSymbols) return;
-
-  const allFunctionsAndMethods = getFilteredSymbols(docSymbols);
-  const potentialFunctions = allFunctionsAndMethods.filter((s) => s.range.intersection(range));
-  const smallestRangeFunction = potentialFunctions.reduce((prev, curr) => {
-    const prevRange = prev.range.end.line - prev.range.start.line;
-    const currRange = curr.range.end.line - curr.range.start.line;
-    return currRange < prevRange ? curr : prev;
-  });
-
-  return Promise.resolve(smallestRangeFunction);
+  return {
+    name: enclosingFn.name,
+    range: enclosingFnRange,
+    functionType: enclosingFn['function-type'],
+    'file-type': extension,
+    content: document.getText(enclosingFnRange),
+  } as FnToRefactor;
 }
