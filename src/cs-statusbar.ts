30f4f70d586791cb1b526ef8f76cfb1cecf24a45
Johan Lindbergh <johan.lindbergh@codescene.com>
2024-02-14 15:51:58 +0100

    feat: Status bar icon - initial commit

---
+++ src/cs-statusbar.ts
@@ -0,0 +1,21 @@
+import vscode from 'vscode';
+
+export class CsStatusBar {
+  private readonly statusBarItem: vscode.StatusBarItem;
+
+  constructor() {
+    this.statusBarItem = vscode.window.createStatusBarItem(
+      'codescene.statusBarItem',
+      vscode.StatusBarAlignment.Right,
+      -1
+    );
+    this.statusBarItem.name = 'CodeScene status bar';
+    this.statusBarItem.text = '$(cs-logo)';
+    this.statusBarItem.command = 'codescene.remoteFeaturesView.focus';
+    this.statusBarItem.show();
+  }
+
+  setOnline(online?: boolean) {
+    this.statusBarItem.text = `$(cs-logo) ${online ? 'Online' : ''}`;
+  }
+}
