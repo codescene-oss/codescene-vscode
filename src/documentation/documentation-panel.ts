8f23ee333483fccd5db3065b2261e39014d9f306
Johan Lindbergh <johan.lindbergh@codescene.com>
2024-06-10 17:09:45 +0200

    feat(documentation): Initiate refactoring immediately if finding a suitable function

--- src/documentation/documentation-panel.ts
+++ src/documentation/documentation-panel.ts
@@ -8,7 +8,6 @@ import Reviewer from '../review/reviewer';
 import { getLogoUrl, isDefined } from '../utils';
 import { nonce } from '../webviews/utils';
 import { categoryToDocsCode } from './csdoc-provider';
-import { logOutputChannel } from '../log';
 
 export interface CategoryWithPosition {
   category: string;
@@ -18,7 +17,7 @@ export interface CategoryWithPosition {
 interface DocPanelParams {
   codeSmell: CategoryWithPosition;
   documentUri: Uri;
-  refactoring?: CsRefactoringRequest; // If there already is a refactoring available?
+  request?: CsRefactoringRequest; // If there already is a refactoring available?
 }
 
 type DocPanelState = DocPanelParams & {
@@ -37,10 +36,11 @@ export class DocumentationPanel implements Disposable {
     this.webViewPanel = vscode.window.createWebviewPanel(
       DocumentationPanel.viewType,
       'CodeScene',
-      { viewColumn: ViewColumn.Beside, preserveFocus: true },
+      { viewColumn: ViewColumn.Beside },
       {
         enableScripts: true,
         localResourceRoots: [Uri.joinPath(extensionUri, 'out'), Uri.joinPath(extensionUri, 'assets')],
+        retainContextWhenHidden: true, // Need this to keep the state of the auto-refactor button then moving the webview tab around
       }
     );
     this.webViewPanel.onDidDispose(() => this.dispose(), null, this.disposables);
@@ -48,43 +48,32 @@ export class DocumentationPanel implements Disposable {
   }
 
   private async handleWebViewMessage(message: any) {
+    if (!this.state) return;
     switch (message.command) {
-      case 'initiate-refactoring':
-        this.initiateRefactoring();
+      case 'show-refactoring':
+        this.showRefactoring(this.state);
         return;
       case 'goto-function-location':
-        this.goToFunctionLocation();
+        this.goToFunctionLocation(this.state);
         return;
     }
   }
 
-  private initiateRefactoring() {
-    if (!this.state) return;
-    if (this.state.refactoring) {
-      void vscode.commands.executeCommand('codescene.presentRefactoring', this.state.refactoring, ViewColumn.Active);
-      return;
-    }
-
-    if (this.state.fnToRefactor && this.state.document) {
-      void vscode.commands.executeCommand(
-        'codescene.requestAndPresentRefactoring',
-        this.state.document,
-        this.state.fnToRefactor,
-        this.state.codeSmell.position.line
-      );
+  private showRefactoring(state: DocPanelState) {
+    if (state.request) {
+      void vscode.commands.executeCommand('codescene.presentRefactoring', state.request, ViewColumn.Active);
     }
   }
 
-  private goToFunctionLocation() {
-    if (!this.state) return;
-    const uri = this.state.documentUri;
+  private goToFunctionLocation(state: DocPanelState) {
+    const uri = state.documentUri;
 
     /**
      * Need to do this because the goToLocations command expects a proper vscode.Position,
      * not a {line, character} object which we might get when coming from a diagnostic
      * target uri (where args are encoded as query params). The uri is fine though ¯\_(ツ)_/¯
      */
-    const { line, character } = this.state.codeSmell.position;
+    const { line, character } = state.codeSmell.position;
     const position = new vscode.Position(line, character);
 
     const location = new vscode.Location(uri, position);
@@ -96,31 +85,28 @@ export class DocumentationPanel implements Disposable {
       codeSmell: { category, position },
       documentUri,
     } = params;
+
+    // Set webview state
     this.state = params;
+    if (this.state.request) {
+      this.state.document = this.state.request.document;
+      this.state.fnToRefactor = this.state.request.fnToRefactor;
+    }
 
     const title = category;
     this.webViewPanel.title = `CodeScene - ${title}`;
-    
+
     const webviewScript = this.getUri('out', 'documentation', 'webview-script.js');
     const markdownLangCss = this.getUri('assets', 'markdown-languages.css');
     const documentationCss = this.getUri('assets', 'documentation-styles.css');
     const codiconsUri = this.getUri('out', 'codicons', 'codicon.css');
     const csLogoUrl = await getLogoUrl(this.extensionUri.fsPath);
 
-    if (CsExtensionState.acePreflight) {
-      // Asynchronously open doc and find refactorable function, then posting a message back to the
-      // webview to show the refactor button. (see webview-script.ts)
-      void vscode.workspace.openTextDocument(documentUri).then((document) => {
-        void this.findRefactorableFunction(document, position.line).then((fnToRefactor) => {
-          if (!this.state) return;
-          this.state.document = document;
-          this.state.fnToRefactor = fnToRefactor;
-          void this.webViewPanel.webview.postMessage({
-            command: 'show-refactor-button',
-            args: [isDefined(fnToRefactor)],
-          });
-        });
-      });
+    let hideRefactorButton = true;
+    if (this.state.document && this.state.fnToRefactor) {
+      hideRefactorButton = false;
+    } else {
+      this.attemptRefactoring(documentUri, position);
     }
 
     const docsContent = await this.docsForCategory(category);
@@ -148,7 +134,7 @@ export class DocumentationPanel implements Disposable {
         <script type="module" nonce="${nonce()}" src="${webviewScript}"></script>
         <h1><img src="data:image/png;base64,${csLogoUrl}" width="64" height="64" align="center"/>&nbsp; ${title}</h1>
 
-        ${this.documentationHeaderContent(documentUri, position)}
+        ${this.documentationHeaderContent(hideRefactorButton, documentUri, position)}
         <br>
         ${docsContent}
     </body>
@@ -157,14 +143,14 @@ export class DocumentationPanel implements Disposable {
     `;
   }
 
-  private documentationHeaderContent(uri: Uri, position: vscode.Position) {
+  private documentationHeaderContent(hideRefactorButton: boolean, uri: Uri, position: vscode.Position) {
     const fileName = path.basename(uri.path);
     return /*html*/ `
     <div class="documentation-header">
       <div id="function-location" title="Go to line ${position.line + 1} in ${fileName}">
         <span>${fileName}</span><span class="line-no">:L${position.line + 1}</span>
       </div>
-      <vscode-button id="refactoring-button" class="hidden">
+      <vscode-button id="refactoring-button" class="${hideRefactorButton ? 'hidden' : ''}">
         <span slot="start" class="codicon codicon-sparkle"></span>
         Auto-refactor
       </vscode-button>
@@ -173,10 +159,33 @@ export class DocumentationPanel implements Disposable {
   `;
   }
 
+  /**
+   * This function attempts to find a refactorable function in the document at the given line.
+   * If found, it will post for a refactoring, save the request reference, and at the same time
+   * send a message to the webview to show the refactor button.
+   */
+  private attemptRefactoring(documentUri: Uri, position: vscode.Position) {
+    if (CsExtensionState.acePreflight) {
+      // Asynchronously open doc and find refactorable function, then posting a message back to the
+      // webview to show the refactor button. (see webview-script.ts)
+      void vscode.workspace.openTextDocument(documentUri).then((document) => {
+        void this.findRefactorableFunction(document, position.line).then((fnToRefactor) => {
+          if (!this.state) return;
+          this.state.document = document;
+          this.state.fnToRefactor = fnToRefactor;
+          void this.initiateRefactoring(this.state);
+          void this.webViewPanel.webview.postMessage({
+            command: 'show-refactor-button',
+            args: [isDefined(fnToRefactor)],
+          });
+        });
+      });
+    }
+  }
+
   private async findRefactorableFunction(document: vscode.TextDocument, lineNo: number) {
     const diagnostics = await Reviewer.instance.review(document).diagnostics;
     const diagnosticsAtLine = diagnostics.filter((d) => d.range.start.line === lineNo);
-
     const fnToRefactor = await vscode.commands.executeCommand<FnToRefactor | undefined>(
       'codescene.getFunctionToRefactor',
       document,
@@ -184,6 +193,16 @@ export class DocumentationPanel implements Disposable {
       lineNo
     );
     return fnToRefactor;
+  }
+
+  private async initiateRefactoring(state: DocPanelState) {
+    if (state.fnToRefactor && state.document) {
+      state.request = await vscode.commands.executeCommand(
+        'codescene.initiateRefactoringForFunction',
+        state.document,
+        state.fnToRefactor
+      );
+    }
   }
 
   private async docsForCategory(category: string) {
@@ -203,15 +222,15 @@ export class DocumentationPanel implements Disposable {
     this.disposables.forEach((d) => d.dispose());
   }
 
-  static createOrShow({ codeSmell, documentUri, refactoring, extensionUri }: DocPanelParams & { extensionUri: Uri }) {
+  static createOrShow({ codeSmell, documentUri, request, extensionUri }: DocPanelParams & { extensionUri: Uri }) {
     if (DocumentationPanel.currentPanel) {
-      void DocumentationPanel.currentPanel.updateWebView({ codeSmell, documentUri, refactoring });
+      void DocumentationPanel.currentPanel.updateWebView({ codeSmell, documentUri, request });
       DocumentationPanel.currentPanel.webViewPanel.reveal(undefined, true);
       return;
     }
 
     // Otherwise, create a new web view panel.
     DocumentationPanel.currentPanel = new DocumentationPanel(extensionUri);
-    void DocumentationPanel.currentPanel.updateWebView({ codeSmell, documentUri, refactoring });
+    void DocumentationPanel.currentPanel.updateWebView({ codeSmell, documentUri, request });
   }
 }
