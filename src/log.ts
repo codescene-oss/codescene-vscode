776daa064dc2781f17bea6ab0e0a3901377b4f49
Johan Lindbergh <johan.lindbergh@codescene.com>
2024-10-10 14:03:59 +0200

    feat(control-center): First implementation of control-center panel from the new  UX

--- src/log.ts
+++ src/log.ts
@@ -3,4 +3,10 @@ import * as vscode from 'vscode';
 
 const logOutputChannel = vscode.window.createOutputChannel('CodeScene Log', { log: true });
 
+export function registerShowLogCommand(context: vscode.ExtensionContext) {
+  void vscode.commands.registerCommand('codescene.showLogOutput', () => {
+    logOutputChannel.show();
+  });
+}
+
 export { logOutputChannel };
