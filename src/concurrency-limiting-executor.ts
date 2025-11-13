0a0bf01f624bc8c90ecce3826ddd1a55219f9ad6
vemv <vemv@users.noreply.github.com>
2025-11-13 19:14:20 +0100

    chore: fix a test

--- src/concurrency-limiting-executor.ts
+++ src/concurrency-limiting-executor.ts
@@ -17,9 +17,11 @@ export class ConcurrencyLimitingExecutor implements Executor {
   constructor(executor: Executor, maxConcurrency?: number) {
     this.executor = executor;
 
-    // Temporarily disabled - might contribute to ERR_CHILD_PROCESS_STDIO_MAXBUFFER errors
+    // Temporarily disabled to avoid amplifying performance issues
     // this.maxConcurrency = maxConcurrency ?? Math.max(1, os.cpus().length - 1); // Use <cores - 1> in order to not saturate the user's resources
-    this.maxConcurrency = 1;
+    // Idea: use 1 third of available cores.
+
+    this.maxConcurrency = maxConcurrency ?? 1;
   }
 
   async execute(command: Command | Task, options: ExecOptions = {}, input?: string): Promise<ExecResult> {
