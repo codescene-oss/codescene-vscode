e3c406ee94e6921237ff561b983cdd7ff6ccfd84
vemvcs <victor.valenzuela+gh@codescene.com>
2026-01-01 22:23:47 +0100

    chore: integration test - improve error reporting (#272)
    By observing the EventEmitter, we can be notified of any thrown errors.
    
    Co-authored-by: vemv <vemv@users.noreply.github.com>

--- src/test/stubs/event-emitter-stub.ts
+++ src/test/stubs/event-emitter-stub.ts
@@ -1,5 +1,23 @@
-export class EventEmitterStub {
-  event = () => ({ dispose: () => {} });
-  fire() {}
-  dispose() {}
+export class EventEmitterStub<T = any> {
+  private listeners: Array<(e: T) => any> = [];
+
+  event = (listener: (e: T) => any) => {
+    this.listeners.push(listener);
+    return {
+      dispose: () => {
+        const index = this.listeners.indexOf(listener);
+        if (index > -1) {
+          this.listeners.splice(index, 1);
+        }
+      }
+    };
+  };
+
+  fire(data: T) {
+    this.listeners.forEach(listener => listener(data));
+  }
+
+  dispose() {
+    this.listeners = [];
+  }
 }
