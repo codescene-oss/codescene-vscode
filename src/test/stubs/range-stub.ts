4ea1be9e8051ef3369d81ff3df9a5185c75fcfa0
vemv <vemv@users.noreply.github.com>
2026-01-06 15:17:58 +0100

    chore: improve fns-to-refactor.test.ts

--- src/test/stubs/range-stub.ts
+++ src/test/stubs/range-stub.ts
@@ -6,4 +6,38 @@ export class RangeStub {
     this.start = { line: startLine, character: startCharacter };
     this.end = { line: endLine, character: endCharacter };
   }
+
+  intersection(range: RangeStub): RangeStub | undefined {
+    const startLine = Math.max(this.start.line, range.start.line);
+    const endLine = Math.min(this.end.line, range.end.line);
+
+    if (startLine > endLine) {
+      return undefined;
+    }
+
+    let startChar: number;
+    let endChar: number;
+
+    if (startLine === this.start.line && startLine === range.start.line) {
+      startChar = Math.max(this.start.character, range.start.character);
+    } else if (startLine === this.start.line) {
+      startChar = this.start.character;
+    } else {
+      startChar = range.start.character;
+    }
+
+    if (endLine === this.end.line && endLine === range.end.line) {
+      endChar = Math.min(this.end.character, range.end.character);
+    } else if (endLine === this.end.line) {
+      endChar = this.end.character;
+    } else {
+      endChar = range.end.character;
+    }
+
+    if (startLine === endLine && startChar >= endChar) {
+      return undefined;
+    }
+
+    return new RangeStub(startLine, startChar, endLine, endChar);
+  }
 }
